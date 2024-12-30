488e1ebea9c3492ae417e17cb9748275eaf632dc
Yalfmon0307 <montesyalfred0@gmail.com>
2024-12-29 19:50:46 -0600

    inicializando el servidor express

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`servidor levantado en http://localhost:${port}`);
+})
